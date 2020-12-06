f282ffdd6d3f5372b69868ce5b3d825727c3032a
everttigchelaar <everttigchelaar@gmail.com>
2020-12-06 12:46:28 +0100

    Scripts

--- microk8s/configure.sh
+++ microk8s/configure.sh
@@ -1,6 +1,4 @@
 sudo usermod -a -G microk8s $USER
 sudo chown -f -R $USER ~/.kube
-echo "alias kubectl='microk8s kubectl'" >> ~/.bash_aliases
-su - $USER
 microk8s start
 microk8s enable ingress dns
