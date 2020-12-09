b7bda747fd71a71c4bd82472962cae21cf33719e
Evert Tigchelaar <everttigchelaar@gmail.com>
2020-12-09 21:07:23 +0100

    Scripts

--- scripts/login-maria-pod.sh
+++
@@ -1,2 +0,0 @@
-POD_NAME=$(kubectl get pods -n general -o jsonpath='{.items[*].metadata.name}' | grep maria
-kubectl exec -it $POD_NAME -n general -- sh
--- scripts/service/setup-auth-service.sh
+++ scripts/service/setup-auth-service.sh
@@ -1,8 +1,11 @@
 #Create JWT secret
 ## Generate random gerated base64 encoded jwtsecret and store it in the jwt-secret (replace myjwtsecret value)
 ## For your local cluster just use the value from application.properties
-echo $(tr -dc A-Za-z0-9 < /dev/urandom | dd bs=100 count=1 2>/dev/null) | JWT_SECRET_VALUE=$(base64)
-kubectl create secret generic jwt-secret -n general --from-literal=jwtsecret='${JWT_SECRET_VALUE}'
+#echo $(tr -dc A-Za-z0-9 < /dev/urandom | dd bs=100 count=1 2>/dev/null) | JWT_SECRET_VALUE=$(base64)
+#kubectl create secret generic jwt-secret -n general --from-literal=jwtsecret='${JWT_SECRET_VALUE}'
+
+JWT_SECRET_VALUE=$(tr -dc A-Za-z0-9 < /dev/urandom | dd bs=100 count=1 2>/dev/null | base64)
+kubectl create secret generic jwt-secret -n general --from-literal=jwtsecret="$JWT_SECRET_VALUE"
 
 #Create secret for auth-server
 kubectl create secret generic auth-secret -n general \
